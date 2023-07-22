3988e63d701228be0276833fbe35dfbcbeb9d6ff
CodeStrik <codestrik@gmail.com>
2023-07-22 10:16:04 +0200

    All comments translated

--- agent-server.py
+++ agent-server.py
@@ -2,57 +2,60 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import paho.mqtt.client as mqtt
 import json
 
-class ProcessRequest(BaseHTTPRequestHandler):
+class ServerProcessRequest(BaseHTTPRequestHandler):
+    
+    # Incoming POST requests handler
     def do_POST(self):
         content_type = self.headers['Content-Type']
         content_length = int(self.headers['Content-Length'])
         request_data = self.rfile.read(content_length)
-        print(f"Recibido mensaje JSON-LD:\n")
+        print(f"Received JSON-LD message:\n")
         response = self.handle_notification(content_type, request_data)
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
         self.end_headers()
         self.wfile.write(response.encode())
 
+    # It handles message body formatting and performs the publishing to the MQTT broker
     def handle_notification(self, content_type, request_data):
         if content_type == 'application/ld+json':
             notification_data = json.loads(request_data.decode('utf-8'))
-            # Formatea la notificación para mostrarla en formato JSON
+            # It formats the notification to display it in JSON format
             formatted_notification = json.dumps(notification_data, indent=4)
-            # Imprimir cuerpo de la notificación
+            # Print notification body
             print(formatted_notification)
             
-            id_dispositivo = notification_data["data"][0]["id"]     # Guarda el id del dispositivo
+            device_id = notification_data["data"][0]["id"]     # Save the device ID
 
-            json_data = notification_data["data"]   # almacenamos todos los datos del dispositivo
-            atributos_dispositivos_formateados = []
+            json_data = notification_data["data"]   # We store all the device data
+            formatted_device_attributes = []
 
-            # Formatear los atributos de cada dispositivo y almacenarlos en la lista
-            for dispositivo in json_data:
-                atributos_formateados = []
-                for atributo, valor in dispositivo.items():
-                    if atributo != "id" and atributo != "type":
-                        atributos_formateados.append(f"{atributo.capitalize()} = {valor['value']}")
-                atributos_dispositivos_formateados.append(", ".join(atributos_formateados))
-            # Publicación de los datos:
-            # Configuración del cliente MQTT
+            # Format the attributes of each device and store them in the list
+            for device in json_data:
+                formatted_attributes = []
+                for attribute, value in device.items():
+                    if attribute != "id" and attribute != "type":
+                        formatted_attributes.append(f"{attribute.capitalize()} = {value['value']}")
+                formatted_device_attributes.append(", ".join(formatted_attributes))
+            # Data publishing:
+            # MQTT client configuration
             broker_address = "localhost"  # Cambiar por la dirección de tu broker MQTT
             client = mqtt.Client("Agent")
 
-            # Configuración de usuario y contraseña para la conexión
+            # Username and password configuration for the connection
             username = "admin" 
             password = "strik"  
             client.username_pw_set(username, password)
 
-            # Conexión al broker
+            # Connecting to the broker
             client.connect(broker_address)
 
-            # Publicación de un mensaje
-            topic = id_dispositivo 
-            message = str(atributos_dispositivos_formateados)
+            # Message publishing
+            topic = device_id 
+            message = str(formatted_device_attributes)
             client.publish(topic, message)
 
-            # Desconexión del cliente
+            # Client disconnection
             client.disconnect()
 
         return 'OK'
@@ -60,8 +63,8 @@ class ProcessRequest(BaseHTTPRequestHandler):
 def main():
     host = '0.0.0.0'
     port = 8888
-    agentserver = HTTPServer((host, port), ProcessRequest)
-    print(f"Servidor escuchando en http://{host}:{port}/notify")
+    agentserver = HTTPServer((host, port), ServerProcessRequest)
+    print(f"Server listening at http://{host}:{port}/notify")
     agentserver.serve_forever()
 
 if __name__ == '__main__':
--- test/suscriptor-mqtt.py
+++ test/suscriptor-mqtt.py
@@ -1,35 +1,43 @@
 import paho.mqtt.client as mqtt
+import argparse
 
-def suscription():
-    # Configuración del cliente MQTT
+def suscription(topic):
+    # MQTT client configuration
     broker_address = "localhost"  
-    client = mqtt.Client("suscriptor")  
+    client = mqtt.Client("Subscriber")  
 
-    # Configuración de usuario y contraseña para la conexión
+    # Username and password configuration for the connection
     username = "admin"  
     password = "strik"  
     client.username_pw_set(username, password)
 
-    # Definición de una función para procesar los mensajes recibidos
+    # Definition of a function to process received messages
     def on_message(client, userdata, message):
-        print("Mensaje recibido en el tópico: ", message.topic)
-        print("Contenido del mensaje: ", message.payload.decode())
+        print("Message received on the topic: ", message.topic)
+        print("Message content: ", message.payload.decode())
 
-    # Configuración de la función de recepción de mensajes
+    # Configuration of the message reception function
     client.on_message = on_message
 
-    # Conexión al broker
+    # Connecting to the broker
     client.connect(broker_address)
 
-    # Suscripción a un tópico específico
-    topic = "urn:ngsi-ld:PruebaDispositivo:003"
+    # Subscription to a specific topic
+    #topic = "urn:ngsi-ld:PruebaDispositivo:003"     # Each topic will have the name of the device's URN within Orion-LD
     client.subscribe(topic)
 
-    # Mantener el cliente a la escucha de mensajes entrantes
+    # Keeping the client listening for incoming messages
     client.loop_forever()
 
 def main():
-    suscription()
+    parser = argparse.ArgumentParser(description="Script to subscribe to an MQTT topic")
+    parser.add_argument("-t", "--topic", required=True, help="MQTT topic name (Device URN), example: urn:ngsi-ld:Device:001")
+    parser.add_argument("-u", "--user", required=True, help="User to access the MQTT broker, example: user1")
+    parser.add_argument("-pw", "--password", required=True, help="Password to access the MQTT broker, example: 1234")
+    args = parser.parse_args()
+
+    # We call the function, passing the argument received from the terminal
+    suscription(args.topic)
 
 if __name__ == '__main__':
     main()
